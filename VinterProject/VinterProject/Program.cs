faf664f016e921489dd96b339ec45dc384c6fa57
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2022-02-01 14:54:04 +0100

    Start

--- VinterProject/VinterProject/Program.cs
+++ VinterProject/VinterProject/Program.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using Raylib_cs;
 using System.Numerics;
 
+Raylib.SetTargetFPS(60);
+Raylib.InitWindow(800, 600, "Go");
+
+String Location = "Beginning";
+
+bool Playing = true;
+
+if (Playing = true)
+{
+    main();
+}
+
 static void main()
 {
 
 
-
+    if (something = "yes")
+    {
+        Playing = false;
+    }
 }
