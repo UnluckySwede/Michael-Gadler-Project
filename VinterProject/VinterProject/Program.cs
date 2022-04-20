179bb75663468fda903b8d5de1173b9a43fcac88
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2022-04-20 13:28:23 +0200

    Some locations working

--- VinterProject/VinterProject/Program.cs
+++ VinterProject/VinterProject/Program.cs
@@ -20,9 +20,9 @@ static bool Main(bool playing)
     string replay = "placeholder";
     string choice = "notmade";
     int location = 0;
-    string[] names = { "forrest", "woods", "river" };
-    string[] desc = { "You look around and see a path", "Also green", "blue" };
-    string[] allowedPaths = { "a crossroad", "the beginning and a river", "a crossroad" };
+    List<string> names = new List<string>() { "glade", "forest", "river", "ruin" };
+    string[] desc = { "You look around and see that you're in a open area surrounded by forest", "Also green", "blue", "You arrive at a crumbled old ruin with nothing noteworthy at first sight" };
+    string[] allowedPaths = { "a crossroad", "the glade and a river", "a crossroad" };
     string[] plural = { "a", "multiple", "a" };
 
     while (playing == true)
@@ -33,12 +33,33 @@ static bool Main(bool playing)
         Console.WriteLine($"{desc[location]}");
         System.Console.WriteLine($"You see {plural[location]} path leading to {allowedPaths[location]}");
 
-        choice = Console.ReadLine();
+        choice = Console.ReadLine().ToLower();
 
-        if (choice == "river")
+        if (names.Contains(choice) && choice == "forest" && allowedPaths[location] == "a crossroad")
         {
-            System.Console.WriteLine("yes");
+            System.Console.WriteLine("working forest");
+            location = 1;
+            choice = "notmade";
         }
+
+        if (names.Contains(choice) && choice == "river")
+        {
+            System.Console.WriteLine("working river");
+            location = 2;
+            choice = "notmade";
+        }
+
+        if (names.Contains(choice) && choice == "glade" && allowedPaths[location] == "the glade and a river")
+        {
+            System.Console.WriteLine("working glade");
+            location = 0;
+            choice = "notmade";
+        }
+
+        //  if (names.Contains(choice) && location  
+
+
+
 
 
         // System.Console.WriteLine("You look around and see nothing");
@@ -103,4 +124,9 @@ static bool Main(bool playing)
 
     return playing;
 
+}
+
+void tryparse(int v)
+{
+    throw new NotImplementedException();
 }
